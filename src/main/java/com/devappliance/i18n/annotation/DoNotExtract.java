979be23a99d8a68553ea81dc1443106044455397
Gibah Joseph <gibahjoe@gmail.com>
2020-04-10 13:16:46 +0100

    initial commit

---
+++ src/main/java/com/devappliance/i18n/annotation/DoNotExtract.java
@@ -0,0 +1,16 @@
+package com.devappliance.i18n.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Gibah Joseph
+ * Email: gibahjoe@gmail.com
+ * Apr, 2020
+ */
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE})
+@Retention(RetentionPolicy.SOURCE)
+public @interface DoNotExtract {
+}
---
+++ src/main/java/com/devappliance/i18n/annotation/Extract.java
@@ -0,0 +1,16 @@
+package com.devappliance.i18n.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Gibah Joseph
+ * Email: gibahjoe@gmail.com
+ * Apr, 2020
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.SOURCE)
+public @interface Extract {
+}
